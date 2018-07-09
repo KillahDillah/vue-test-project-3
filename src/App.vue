efb545a045c724c96af2ed80cdf154de83bd0f98
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-07-09 14:03:19 -0700

    tutorial did not specify removing ProductList component from App.vue and replacing it with ManageProducts component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
 <template>
   <!-- when using a component in a template Vue.js expects the DOM element to be
   lowercased and dasherized. e.g. ProductList -> product-list -->
-
-  <product-list></product-list>
+  <!-- <product-list></product-list> -->
+  <manage-products></manage-products>
 </template>
 
 <script>
 import ProductList from './components/ProductList'
+import ManageProducts from './components/ManageProducts'
+import SaveProductForm from './components/SaveProductForm'
 
 export default {
   components: {
     // With ES6 we can use the Object Literal Shorthand Syntax to save us some
     // keystrokes
-    ProductList
+    ProductList,
+    ManageProducts,
+    SaveProductForm
   }
 }
 </script>
