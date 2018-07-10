49bd97c41d731b97b832cc7a398c7d53e0c8854c
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-07-09 22:33:13 -0700

    customize routes and display

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+import App from './App'
+import ProductCatalog from './components/ProductCatalog'
+import ManageProducts from './components/ManageProducts'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/home', alias: '/', component: ManageProducts },
+  { path: '/product-catalog', component: ProductCatalog }
+]
+
+// Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
