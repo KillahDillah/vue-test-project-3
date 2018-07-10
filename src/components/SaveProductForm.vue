9e5caace9c6123e240b7bf72b6c3898462924e4d
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-07-10 09:52:39 -0700

    ternary statement completed for update/add button

--- src/components/SaveProductForm.vue
+++ src/components/SaveProductForm.vue
@@ -13,8 +13,8 @@
       <label for="price">Price</label>
       <input type="number" v-model="product.price" class="form-control" id="price" placeholder="Enter Price" number>
     </div>
-    <button type="submit" v-on:click.prevent="onSubmit" v-if="this.product.id" class="btn btn-primary">Edit product</button>
-    <button type="submit" v-on:click.prevent="onSubmit" v-else class="btn btn-primary">Add product</button>
+    <button type="submit" v-on:click.prevent="onSubmit" class="btn btn-primary">{{product.id ? 'Update' : 'Add'}}</button>
+    <!-- <button type="submit" v-on:click.prevent="onSubmit" v-else class="btn btn-primary">Add product</button> -->
     <button v-if="this.product.id" v-on:click.prevent="cancel" class="btn btn-primary">Cancel</button>
   </form>
 </template>
@@ -23,13 +23,37 @@
 
 export default {
   props: ['product'], 
+  data() {
+    return {
+      formErrors: {}
+    }
+  },
+  watch: {
+    'product.id' () {
+      this.formErrors = {}
+    }
+  },
   methods: {
     onSubmit () {
-      this.$emit('submit', this.product)
+      if (this.validate()) {
+        this.$emit('submit', this.product)
+      }
     },
     cancel () {
+      this.formErrors = {}
       this.$emit('cancel')
-    }
+    },
+    validate () {
+      const errors = {}
+      if (!this.product.name) {
+        errors.name = 'Name is required'
+      }
+      if (!this.product.price) {
+        errors.price = 'Price is required'
+      }
+      this.formErrors = errors
+      return Object.keys(errors).length === 0
+    },
   }
 }
 </script>
