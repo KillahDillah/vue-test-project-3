32ea7a5840ef4832eeeb24637dc7719573101d4e
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-07-10 10:43:01 -0700

    files added missing from last commit (CRUD)

--- src/vuex/modules/products/index.js
+++ src/vuex/modules/products/index.js
@@ -1,4 +1,11 @@
 import * as getters from './getters'
+import * as actions from './actions'
+
+import {
+  CREATE_PRODUCT,
+  UPDATE_PRODUCT,
+  DELETE_PRODUCT
+} from './mutation-types'
 
 // initial state
 const initialState = {
@@ -27,10 +34,29 @@ const initialState = {
 
 // mutations
 const mutations = {
+  [CREATE_PRODUCT] (state, product) {
+    state.all.push(product)
+  },
+
+  [UPDATE_PRODUCT] (state, product) {
+    const index = state.all.findIndex((p) => p.id === product.id)
+
+    if (index !== -1) {
+      // We need to replace the array entirely so that vue can recognize
+      // the change and re-render entirely.
+      // See http://vuejs.org/guide/list.html#Caveats
+      state.all.splice(index, 1, product)
+    }
+  },
+
+  [DELETE_PRODUCT] (state, productId) {
+    state.all = state.all.filter(p => p.id !== productId)
+  }
 }
 
 export default {
   state: { ...initialState },
   getters,
+  actions,
   mutations
 }
