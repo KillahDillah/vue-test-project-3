5890fdf0a4609c6249c6736cbea3f8f9d558b7ce
Leah Miller <killahdillah@Leahs-MacBook-Pro.local>
2018-07-09 22:58:24 -0700

    vuex installed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <!-- src/App.vue -->
 <template>
-  <!-- when using a component in a template Vue.js expects the DOM element to be
-  lowercased and dasherized. e.g. ProductList -> product-list -->
-  <!-- <product-list></product-list> -->
-  <!-- <manage-products></manage-products> -->
-
   <div>
     <header class="app-header">
       <app-nav></app-nav>
@@ -16,22 +11,12 @@
 </template>
 
 <script>
-// import ProductList from './components/ProductList'
-// import ManageProducts from './components/ManageProducts'
-// import SaveProductForm from './components/SaveProductForm'
 import AppNav from './components/AppNav'
 
 export default {
     components: {
     AppNav
   }
-  // components: {
-  //   // With ES6 we can use the Object Literal Shorthand Syntax to save us some
-  //   // keystrokes
-  //   ProductList,
-  //   ManageProducts,
-  //   SaveProductForm
-  // }
 }
 </script>
 
